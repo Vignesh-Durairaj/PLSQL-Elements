b3a4746da86d32750f19c8a30153784b2609c8b9
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-04-08 10:45:45 +0530

    Updated the setup details for the new DB Objects

--- Oracle/Setup/Run.sql
+++ Oracle/Setup/Run.sql
@@ -6,6 +6,8 @@ SET DEFINE OFF;
 @../Functions/get_manager_id.sql
 @../Functions/get_employee_grade.sql
 
+@../Views/v_duplicate_department.sql
+
 @../Procedures/p_remove_department.sql
 
 @../Packages/crypto.sql
